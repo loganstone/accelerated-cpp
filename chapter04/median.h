c9806e52132f70328631c7cc28dd9c4f1cf331b3
Hyunsuk Lee <hs.lee.logan@gmail.com>
2018-12-27 18:39:18 +0900

    Begin chapter 8

--- chapter04/median.h
+++ chapter04/median.h
@@ -3,8 +3,22 @@
 #ifndef MEDIAN_H_
 #define MEDIAN_H_
 
+#include <algorithm>
+#include <stdexcept>
 #include <vector>
 
-double Median(std::vector<double>);
+template<class T>
+T Median(std::vector<T> vec) {
+  typedef typename std::vector<T>::size_type vec_sz;
+  vec_sz size = vec.size();
+
+  if (size == 0) {
+    throw std::domain_error("median of an empty std::vector");
+  }
+  std::sort(vec.begin(), vec.end());
+  vec_sz mid = size / 2;
+  bool is_even = size % 2 == 0;
+  return is_even ? (vec[mid] + vec[mid - 1]) / 2 : vec[mid];
+}
 
 #endif
